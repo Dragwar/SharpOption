4fc6d2326594ffbff21eb6c6ac5d9e592eb4ff11
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-06-02 00:38:10 -0500

    added support for query syntax

---
+++ src/SharpOption.Core.Tests/MonadicExtensionTests.cs
@@ -0,0 +1,143 @@
+﻿using SharpOption.Core.Monodic;
+using static SharpOption.Core.Option;
+
+namespace SharpOption.Core.Tests;
+public class MonadicExtensionTests
+{
+	[Fact]
+	public void Select_Operator()
+	{
+		// Arrange
+		var op1 = Some(5);
+
+		// Act
+		var op =
+			from first in op1
+			select first;
+
+		// Assert
+		Assert.True(op.IsSome);
+		Assert.Equal(5, op.Value);
+	}
+
+	[Fact]
+	public void Select_Into_Operator()
+	{
+		// Arrange
+		var op1 = Some(5);
+		var op2 = Some(1);
+
+		// Act
+		var op =
+			from first in op1
+			select first into newFirst  // Can be used to separate variables into scopes (only newFirst is visible to the clauses below)
+			from second in op2          // Also if there were any variables above this line, they won't be in the next clause' scope
+			select second + newFirst;
+
+		// Assert
+		Assert.True(op.IsSome);
+		Assert.Equal(6, op.Value);
+	}
+
+	[Fact]
+	public void Where_Operator_True_Condition()
+	{
+		// Arrange
+		var op1 = Some(5);
+
+		// Act
+		var op =
+			from first in op1
+			where first > 1
+			select first;
+
+		// Assert
+		Assert.True(op.IsSome);
+		Assert.Equal(5, op.Value);
+	}
+
+	[Fact]
+	public void Where_Operator_False_Condition()
+	{
+		// Arrange
+		var op1 = Some(1);
+
+		// Act
+		var op =
+			from first in op1
+			where first > 1
+			select first;
+
+		// Assert
+		Assert.True(op.IsNone);
+	}
+
+	[Fact]
+	public void Where_Operator_None()
+	{
+		// Arrange
+		var op1 = None<int>();
+
+		// Act
+		var op =
+			from first in op1
+			where first > 1
+			select first;
+
+		// Assert
+		Assert.True(op.IsNone);
+	}
+
+	[Fact]
+	public void SelectMany_Operator_One_Levels()
+	{
+		// Arrange
+		var op1 = Some(1);
+
+		// Act
+		var op =
+			from first in op1
+			select Some(first + 2);
+
+		// Assert
+		Assert.Equal(3, op.Value);
+	}
+
+	[Fact]
+	public void SelectMany_Operator_Two_Levels()
+	{
+		// Arrange
+		var op1 = Some(1);
+		var op2 = Some(2);
+
+		// Act
+		var op =
+			from first in op1
+			from second in op2
+			select first + second;
+
+		// Assert
+		Assert.Equal(3, op.Value);
+	}
+
+	[Fact]
+	public void SelectMany_Into_Operator()
+	{
+		// Arrange
+		var op1 = Some(5);
+		var op2 = Some(1);
+		var op3 = Some(2);
+
+		// Act
+		var op =
+			from first in op1
+			from second in op2
+			select first + second into firstResult  // Can be used to separate variables into scopes (only newFirst is visible to the clauses below)
+			from first in op3                       // Also if there were any variables above this line, they won't be in the next clause' scope
+			select first + firstResult;
+
+		// Assert
+		Assert.True(op.IsSome);
+		Assert.Equal(8, op.Value);
+	}
+}
---
+++ src/SharpOption.Core/Monodic/MonadicExtensions.cs
@@ -0,0 +1,34 @@
+﻿using SharpOption.Core.Extensions;
+using static SharpOption.Core.Option;
+
+namespace SharpOption.Core.Monodic;
+public static class MonadicExtensions
+{
+	public static Option<T> Where<T>(this Option<T> option, Func<T, bool> predicate) => option.Filter(predicate);
+	public static Option<T> Where<T>(this T option, Func<T, bool> predicate) where T : notnull => option.Some().Filter(predicate);
+	public static Option<TResult> Select<T, TResult>(this Option<T> option, Func<T, Option<TResult>> selector) => option.Bind(selector);
+	public static Option<TResult> Select<T, TResult>(this T option, Func<T, Option<TResult>> selector) where T : notnull => option is not null ? option.Some().Bind(selector) : None<TResult>();
+
+	public static Option<TResult> SelectMany<TFirst, TSecond, TResult>(
+		this Option<TFirst> first,
+		Func<TFirst, Option<TSecond>> getSecond,
+		Func<TFirst, TSecond, TResult> selector) => first.Bind(f => first.Map(getSecond(f), selector));
+
+	public static async Task<Option<TResult>> SelectMany<TFirst, TSecond, TResult>(
+		this Task<Option<TFirst>> first,
+		Func<TFirst, Task<Option<TSecond>>> getSecond,
+		Func<TFirst, TSecond, TResult> selector)
+	{
+		var firstOption = await first;
+		var res = await firstOption.Match(
+			async f =>
+			{
+				var secondOption = await getSecond(f);
+				var r = secondOption.Bind(s => Some(selector(f, s)));
+				return r;
+			},
+			() => Task.FromResult(None<TResult>())
+		);
+		return res;
+	}
+}
