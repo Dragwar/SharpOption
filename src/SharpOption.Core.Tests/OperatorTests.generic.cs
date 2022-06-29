c800d72807bcdfc92d15d1d10ddb12a2fb9207df
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-06-29 01:23:19 -0500

    added some operator and instance member tests

---
+++ src/SharpOption.Core.Tests/OperatorTests.generic.cs
@@ -0,0 +1,89 @@
+﻿namespace SharpOption.Core.Tests;
+
+public class OperatorTests
+{
+	[Fact]
+	public void Some_Get_Return_Value()
+	{
+		// Arrange
+		var op = 10.Some();
+		var expected = 10;
+
+		// Act
+		var actual = op.Get();
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void None_Get_Throw_ArgumentException()
+	{
+		// Arrange
+		var op = None<int>();
+
+		// Act
+		// Assert
+		Assert.Throws<ArgumentException>(() => op.Get());
+	}
+
+	[Fact]
+	public void Some_Bind_Return_Some()
+	{
+		// Arrange
+		var op = 10.Some();
+		var expected = 11.Some();
+
+		// Act
+		var actual = op.Bind(n => (n + 1).Some());
+
+		// Assert
+		Assert.True(actual.IsSome);
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void Some_Bind_Return_None()
+	{
+		// Arrange
+		var op = 10.Some();
+		var expected = None<int>();
+
+		// Act
+		var actual = op.Bind(_ => None<int>());
+
+		// Assert
+		Assert.False(actual.IsSome);
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void Some_Map_Return_Some()
+	{
+		// Arrange
+		var op = 10.Some();
+		var expected = 11.Some();
+
+		// Act
+		var actual = op.Map(n => n + 1);
+
+		// Assert
+		Assert.True(actual.IsSome);
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void None_Map_Return_None()
+	{
+		// Arrange
+		var op = None<int>();
+		var expected = None<int>();
+
+		// Act
+		var actual = op.Map(n => n + 1);
+
+		// Assert
+		Assert.False(actual.IsSome);
+		Assert.Equal(expected, actual);
+	}
+}
---
+++ src/SharpOption.Core.Tests/OptionInstanceTests.generic.cs
@@ -0,0 +1,55 @@
+﻿namespace SharpOption.Core.Tests;
+
+public class OptionInstanceTests
+{
+	[Fact]
+	public void Some_And_IsSome_True_And_IsNone_False()
+	{
+		// Arrange
+		var op = 10.Some();
+
+		// Act
+		// Assert
+		Assert.True(op.IsSome);
+		Assert.False(op.IsNone);
+	}
+
+	[Fact]
+	public void None_And_IsSome_False_And_IsNone_True()
+	{
+		// Arrange
+		var op = None<int>();
+
+		// Act
+		// Assert
+		Assert.False(op.IsSome);
+		Assert.True(op.IsNone);
+	}
+
+	[Fact]
+	public void Some_And_Returns_Getter_Value()
+	{
+		// Arrange
+		var op = 10.Some();
+		var expected = 10;
+
+		// Act
+		var actual = op.Value;
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void None_And_Getter_Value_Throws_InvalidOperationException()
+	{
+		// Arrange
+		var op = None<int>();
+
+		// Act
+		// Assert
+		Assert.Throws<InvalidOperationException>(() => op.Value);
+	}
+
+	//TODO: move equality/comparison tests here
+}
